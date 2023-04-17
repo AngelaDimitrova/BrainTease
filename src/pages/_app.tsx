77b5e9d7f90ee5eb0482f6fbe2f6c2e678c9aac9
Angela <angela.dimitrova@halicea.com>
2023-04-17 15:26:25 +0200

    Implementing layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
-import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useEffect } from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../styles/globals.css";
+import "../styles/internal.css";
+
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    require("bootstrap/dist/js/bootstrap.bundle.min.js");
+  }, []);
   return <Component {...pageProps} />
 }
---
+++ src/pages/home/index.tsx
@@ -0,0 +1,6 @@
+const HomePage = () => {
+return (
+    <div>A</div>
+)
+}
+export default HomePage
